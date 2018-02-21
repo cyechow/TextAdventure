3e84b203e87172b511a7ec28a47aa8f08462a524
Edith Chow <Edith.Chow@arup.com>
2018-02-21 13:43:47 -0500

    Initial set up of the text based adventure game.

---
+++ adventure.py
@@ -0,0 +1,14 @@
+# TODO: Check whether a number is the sum of two values, returns a boolean indicating whether it is correct
+def CheckAddition():
+
+# TODO: Create dialogue to be displayed to the player that requires injection of some current state within the sentence.
+# e.g. "There are N items left to find." Where N is some value that is changed based on the player's progression through the game.
+def CreateDialogue():
+
+# TODO: Control the main dynamics of the adventure.
+def main():
+
+
+
+# This will start the program:
+main()
